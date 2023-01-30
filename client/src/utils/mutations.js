c424a1c6feb5cec70f7e97bef416824a16f7c517
Clayton McKee <103902212+cmckee120993@users.noreply.github.com>
2023-01-30 10:27:45 +0100

    Trying to connect to mongodb

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,65 +1,78 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN = gql`
-  mutation login($email: String!, $password: String!) {
+  mutation Login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      user {
-        _id
-      }
     }
   }
 `;
 
-export const UPDATE_USER =gql`
-mutation updateUser ($firstName: String, $lastName: String, $email: String, $password: String) {
-  updateUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
-    _id
-    firstName
-    lastName
-    email
+export const UPDATE_USER = gql`
+  mutation updateUser(
+    $firstName: String
+    $lastName: String
+    $email: String
+    $password: String
+  ) {
+    updateUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      _id
+      firstName
+      lastName
+      email
+    }
   }
-}
-`
+`;
 
 export const ADD_USER = gql`
-    mutation addUser (
-        $firstName: String!
-        $lastName: String!
-        $email: String!
-        $password: String!
-        ) {
-        addUser(
-            firstName: $firstName
-            lastName: $lastName
-            email: $email
-            password: $password
-        ) {
-            token
-            user {
-             _id
-            }
-        }
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
     }
+  }
 `;
 
 export const ADD_ORDER = gql`
-mutation addOrder ($orderOwner: String!, $deliveryDate: String!, $orderTotal: Float!, $products: [ProductInput]!) {
-  addOrder(orderOwner: $orderOwner, deliveryDate: $deliveryDate, orderTotal: $orderTotal, products: $products) {
-    _id
-    purchaseDate
-    deliveryDate
-    orderOwner
-    products {
-      name
-      price
-      quantity
+  mutation addOrder(
+    $orderOwner: String!
+    $deliveryDate: String!
+    $orderTotal: Float!
+    $products: [ProductInput]!
+  ) {
+    addOrder(
+      orderOwner: $orderOwner
+      deliveryDate: $deliveryDate
+      orderTotal: $orderTotal
+      products: $products
+    ) {
+      _id
+      purchaseDate
+      deliveryDate
+      orderOwner
+      products {
+        name
+        price
+        quantity
+      }
+      orderTotal
     }
-    orderTotal
   }
-}
 `;
-
 
 // export const ADD_TO_ORDER = gql`
 // mutation AddToOrder($orderId: ID!, $name: String!, $price: Int!, $purchaseQuantity: Int!) {
